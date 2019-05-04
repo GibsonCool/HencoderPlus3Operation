2482219f46f4184a7a83f17aebc8b6d346a81a3c
DoubleX <double@tg10010.com>
2019-05-04 10:43:46 +0700

    多点触控工程创建

---
+++ 16_multi_touch/src/main/java/com/example/doublex/a16_multi_touch/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.doublex.a16_multi_touch
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v7.app.AppCompatActivity
+import android.util.SparseArray
+import com.example.doublex.a16_multi_touch.fragment.MultiTouchFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+    val homeTabs = ArrayList<Class<*>>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        homeTabs.add(MultiTouchFragment::class.java)
+
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager, homeTabs)
+        container.adapter = mSectionsPagerAdapter
+    }
+
+
+    private class SectionsPagerAdapter(fm: FragmentManager, val items: List<Class<*>>) : FragmentPagerAdapter(fm) {
+        private val fragments by lazy { SparseArray<Fragment>(items.size) }
+        override fun getItem(position: Int): Fragment {
+            if (fragments[position] == null) {
+                val instance = items[position].newInstance() as Fragment
+                fragments.put(position, instance)
+            }
+
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return items.size
+        }
+    }
+}
---
+++ 16_multi_touch/src/main/java/com/example/doublex/a16_multi_touch/fragment/MultiTouchFragment.kt
@@ -0,0 +1,26 @@
+package com.example.doublex.a16_multi_touch.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.doublex.a16_multi_touch.R
+
+
+class MultiTouchFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_multi_touch, container, false)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+}
+
+
