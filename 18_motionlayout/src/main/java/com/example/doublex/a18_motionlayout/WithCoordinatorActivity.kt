a18488b95022dc498bace7f785ce39147961cd17
DoubleX <double@tg10010.com>
2019-05-15 19:18:57 +0700

    MotionLayout配合DrawerLayout和CoordinatorLayout使用

--- 18_motionlayout/src/main/java/com/example/doublex/a18_motionlayout/WithCoordinatorActivity.kt
+++ 18_motionlayout/src/main/java/com/example/doublex/a18_motionlayout/WithCoordinatorActivity.kt
@@ -2,10 +2,18 @@ package com.example.doublex.a18_motionlayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
+import kotlinx.android.synthetic.main.activity_coordinator.*
 
-class WithCoordinatorActivity :AppCompatActivity(){
+class WithCoordinatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coordinator)
+
+        app_bar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+
+            motion_layout.progress = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+        })
     }
 }
--- 18_motionlayout/src/main/java/com/example/doublex/a18_motionlayout/WithDrawerActivity.kt
+++ 18_motionlayout/src/main/java/com/example/doublex/a18_motionlayout/WithDrawerActivity.kt
@@ -1,11 +1,30 @@
 package com.example.doublex.a18_motionlayout
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_drawer.*
 
 class WithDrawerActivity :AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer)
+        drawer_layout.addDrawerListener(object : DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(newState: Int) {
+
+            }
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                motion_layout.progress = slideOffset
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+            }
+        })
     }
 }
