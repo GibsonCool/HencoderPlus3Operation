c00ca6b058811fde8cc75c9ce32b491b6cb41216
DoubleX <double@tg10010.com>
2019-05-17 14:39:53 +0700

    多点各自作用--》简单画板功能

--- 16_multi_touch/src/main/java/com/example/doublex/a16_multi_touch/view/MultiTouchView3.kt
+++ 16_multi_touch/src/main/java/com/example/doublex/a16_multi_touch/view/MultiTouchView3.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.util.SparseArray
 import android.view.MotionEvent
 import android.view.View
 import com.example.doublex.a16_multi_touch.Utils
@@ -16,90 +18,65 @@ class MultiTouchView3 : View {
 
 
     private val imageWidth = Utils.dp2px(200f)
-    private val paint by lazy { Paint(Paint.ANTI_ALIAS_FLAG) }
+    private val paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.STROKE
+            strokeWidth = Utils.dp2px(4f)
+            strokeCap = Paint.Cap.ROUND
+            strokeJoin = Paint.Join.ROUND
+        }
+    }
     private var bitmap: Bitmap
+    private val paths = SparseArray<Path>()
 
-    private var offsetX: Float = 0f
-    private var offsetY: Float = 0f
-    private var downX: Float = 0f
-    private var downY: Float = 0f
-    private var imageOffsetX: Float = 0f
-    private var imageOffsetY: Float = 0f
-
-    // 记录追踪的 Point id
-    private var trackingPointerId = 0
 
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
         bitmap = Utils.getAvatar(resources, imageWidth.toInt())
     }
 
+    /**
+     *  多点记录，在每个 DOWN、POINTER_DOWN 事件中记录下每个 pointer 的 id，
+     *  在 MOVE 事件中使⽤用 id 对它们进⾏行行跟踪
+     */
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.actionMasked) {
+            //根据当前时间的actionIndex 获取对应 PointerID。 通过PointerID创建一一对应path放入集合中
+            MotionEvent.ACTION_POINTER_DOWN,
             MotionEvent.ACTION_DOWN -> {
-                //当view接受到第一个point事件，取第一个的Point id
-                trackingPointerId = event.getPointerId(0)
-
-                downX = event.x
-                downY = event.y
-                imageOffsetX = offsetX
-                imageOffsetY = offsetY
-            }
-
-            MotionEvent.ACTION_MOVE -> {
-
-                val index = event.findPointerIndex(trackingPointerId)
-                offsetX = imageOffsetX + event.getX(index) - downX
-                offsetY = imageOffsetY + event.getY(index) - downY
-                invalidate()
-            }
-
-            // 额外⼿手指按下(按下之前已经有别的⼿手指触摸到 View)
-            MotionEvent.ACTION_POINTER_DOWN -> {
-                //获取多点时候当前事件Pointer对应的index
-                val actionIndex = event.actionIndex
-                //根据index 更新当前追踪的Pointer id
-                trackingPointerId = event.getPointerId(actionIndex)
-
-                //以当前触摸点pointer 的 index 为准更新偏移量和初始值
-                downX = event.getX(actionIndex)
-                downY = event.getY(actionIndex)
-                imageOffsetX = offsetX
-                imageOffsetY = offsetY
-            }
-
-            //有⼿手指抬起，但不不是最后⼀一个(抬起之后，仍然还有别的⼿手指在触 摸着 View)
-            MotionEvent.ACTION_POINTER_UP -> {
-                //获取当前抬起手指pointer index 和 id
                 val actionIndex = event.actionIndex
                 val pointerId = event.getPointerId(actionIndex)
-                /**
-                 * 如果当前取消pointer id 是我们之前一直使用追踪的Pointer id 。
-                 * 这个Pointer将移除，则需要从新寻找更新trackingPointerId
-                 */
-                if (pointerId == trackingPointerId) {
-                    /**
-                     * 更新的 trackingPointerId 我们取最后一个
-                     * 如果当前抬起pointer的index 已经是最后一个，那就往前在取一个
-                     */
-                    val newIndex = event.pointerCount - (if (actionIndex == event.pointerCount - 1) 2 else 1)
+                val path = Path()
+                path.moveTo(event.getX(actionIndex), event.getY(actionIndex))
+                paths.append(pointerId, path)
+            }
 
-                    //以当前触摸点pointer 的 index 为准更新偏移量和初始值
-                    trackingPointerId = event.getPointerId(newIndex)
-                    downX = event.getX(newIndex)
-                    downY = event.getY(newIndex)
-                    imageOffsetX = offsetX
-                    imageOffsetY = offsetY
+            //遍历集合-->根据pointerID找到对应的path进行路径记录
+            MotionEvent.ACTION_MOVE -> {
+                for (i in 0 until event.pointerCount) {
+                    val pointerId = event.getPointerId(i)
+                    val path = paths.get(pointerId)
+                    path.lineTo(event.getX(i), event.getY(i))
                 }
+            }
+            //更具PointerID找到对应path移除
+            MotionEvent.ACTION_POINTER_UP,
+            MotionEvent.ACTION_UP -> {
+                val pointerId = event.getPointerId(event.actionIndex)
+                paths.remove(pointerId)
             }
 
         }
-
+        invalidate()
         return true
     }
 
+    /**
+     * 多点绘制
+     */
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        canvas.drawBitmap(bitmap, offsetX, offsetY, paint)
+        for (i in 0 until paths.size()) {
+            val path = paths.valueAt(i)
+            canvas.drawPath(path, paint)
+        }
     }
 }
