a50487899c03679c583b04a35bfcae5c591c09a7
DoubleX <double@tg10010.com>
2019-05-22 17:15:06 +0700

    add 嵌套滑动

---
+++ 20_drag_nestedscroll/src/main/java/com/example/doublex/a20_drag_nestedscroll/nested_scroll/NestingScrollActivity.kt
@@ -0,0 +1,15 @@
+package com.example.doublex.a20_drag_nestedscroll.nested_scroll
+
+import android.os.Bundle
+import android.support.v4.widget.NestedScrollView
+import android.support.v7.app.AppCompatActivity
+import com.example.doublex.a20_drag_nestedscroll.R
+
+
+class NestingScrollActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nested_scroll)
+    }
+}
